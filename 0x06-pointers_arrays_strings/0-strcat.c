cb0b34bb49d902b197cc4bc5927c298da286f67c
Maina James Mutiga <mainajimie@gmail.com>
2022-09-23 12:33:39 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,47 @@
-#include "main.h"                                                                                                                  
-#include <unistd.h>                                                                                                                     
-                                                                                                                                        
-/**                                                                                                                                     
- *  * _putchar - writes the character c to stdout                                                                                          
- *   * @c: The character to print                                                                                                           
- *    *                                                                                                                                      
- *     * Return: On success 1.                                                                                                                
- *      * On error, -1 is returned, and errno is set appropriately.                                                                            
- *       */                                                                                                                                     
-int _putchar(char c)                                                                                                                    
-{                                                                                                                                       
-	        return (write(1, &c, 1));                                                                                                       
-}
+#include "main.h"
 
+
+
+/**
+ *
+ *  *_strcat - concatenates  the string pointed to by @src to
+ *
+ *   * the end of the string pointed to by @dest
+ *
+ *    *@dest: String that will be appended
+ *
+ *     *@src: String to be concatenated upon
+ *
+ *      *
+ *
+ *       * Return: returns poiner to @dest
+ *
+ *        */
+
+
+
+char *_strcat(char *dest, char *src)
+
+{
+
+
+
+		int index = 0, dest_len = 0;
+
+
+
+			while (dest[index++])
+
+						dest_len++;
+
+
+
+				for (index = 0; src[index]; index++)
+
+							dest[dest_len++] = src[index];
+
+
+
+					return (dest);
+
+}
