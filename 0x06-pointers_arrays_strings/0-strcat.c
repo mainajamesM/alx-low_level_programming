a5d68113096ef278821dd0cc32cbd68b5694f6fd
Maina James Mutiga <mainajimie@gmail.com>
2022-09-23 01:06:00 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,45 +1,15 @@
-<<<<<<< HEAD
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- *  * strcat - Concatenates the string pointed to by @src, including the x                                                     
- *   *          null byte, to the end of the string pointed to by @dest.                                                                 
- *    * @dest: A pointer to the string to be concatenated upon.                                                                           
- *     * @src: The source string to be appended to @dest.                                                                                  
- *      *                                                                                                                                   
- *       * Return: A pointer to the destination string @dest.                                                                                
- *        */                                                                                                                                  
-char *_strcat(char *dest, const char *src)                                                                                            
-{                                                                                                                                    
-	        int index = 0, dest_len = 0;                                                                                                 
-		                                                                                                                                     
-		        while (dest[index++])                                                                                                        
-				                dest_len++;                                                                                                          
-			                                                                                                                                     
-			        for (index = 0; src[index]; index++)                                                                                         
-					                dest[dest_len++] = src[index];                                                                                       
-				                                                                                                                                     
-				        return (dest);                                                                                                               
-=======
-#include"main.h"
-
-/**
-*_strcat-concatenates the string pointed to by @src to
-*the end of the string pointed to by @dest
-*@dest:string that will be appended
-*src: string to be concatenated upon
-*
-*Return: returns pointer to @dest
-*/
-char *_strcat(char *dest, char *src)
-{
-int index=0, dest_len=0;
-
-	while(dest[index++])
-		dest_len++;
-	for(index=0; src[index]; index++)
-		dest[dest_len++]=srx[index];
-	return(dest);
->>>>>>> 97bcb7575afc13ba84b953f061826e03f12d3343
+#include "main.h"                                                                                                                  
+#include <unistd.h>                                                                                                                     
+                                                                                                                                        
+/**                                                                                                                                     
+ *  * _putchar - writes the character c to stdout                                                                                          
+ *   * @c: The character to print                                                                                                           
+ *    *                                                                                                                                      
+ *     * Return: On success 1.                                                                                                                
+ *      * On error, -1 is returned, and errno is set appropriately.                                                                            
+ *       */                                                                                                                                     
+int _putchar(char c)                                                                                                                    
+{                                                                                                                                       
+	        return (write(1, &c, 1));                                                                                                       
 }
 
